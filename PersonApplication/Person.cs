1f382ba1faae40747a435bf6f1e55746b8c9b412
K9921 <K9921@student.jamk.fi>
2017-02-09 15:23:18 +0200

    tuntid5

---
+++ PersonApplication/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SocialSecurityNumber { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + SocialSecurityNumber;
+        }
+    }
+}
--- PersonApplication/Program.cs
+++ PersonApplication/Program.cs
@@ -10,6 +10,43 @@ namespace PersonApplication
     {
         static void Main(string[] args)
         {
+            // create a new Person objects
+            Person person1 = new Person { FirstName = "Kirsi", LastName = "Mainio", SocialSecurityNumber = "112312-1232"};
+            Person person2 = new Person { FirstName = "Matti", LastName = "Husso", SocialSecurityNumber = "102210-1432"};
+            Person person3 = new Person { FirstName = "Teppo", LastName = "Taistelija", SocialSecurityNumber = "052306-4232"};
+
+            // create a Persons object
+            Persons myFriends = new Persons();
+
+            // add person objects to persons collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // get one person from Persons collection
+            Person person4 = myFriends.GetPerson(10);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Person not found in that index!");
+            }
+
+            // print collection
+            myFriends.PrintCollection();
+
+            // find person
+            string sotu = "112312-1232";
+            Console.WriteLine("Find sotu: " + sotu);
+            Person person5 = myFriends.FindPerson(sotu);
+            if(person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            } else
+            {
+                Console.WriteLine("Cant find person with that sotu..");
+            }
         }
     }
 }
